1107ee8dd0d2e17160ad98f9080c2a2e3b07fb67
Shi3ofreniR <t40411304@yandex.ru>
2024-05-07 03:30:44 +0700

    dev

--- src/main/java/mod/nero/tpsound/mixin/TpSoundMixin.java
+++ src/main/java/mod/nero/tpsound/mixin/TpSoundMixin.java
@@ -2,6 +2,8 @@ package mod.nero.tpsound.mixin;
 
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.ThrownEnderpearl;
 import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.HitResult;
@@ -12,9 +14,20 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ThrownEnderpearl.class)
 public abstract class TpSoundMixin {
-    @Inject(method = "onHit", at = @At("HEAD"))
-    private void tpsnd(HitResult pResult, CallbackInfo ci){
+    @Inject(method = "onHit", at = @At("TAIL"))
+    private void tpsnd(HitResult pResult, CallbackInfo ci) {
         ThrownEnderpearl pearl = (ThrownEnderpearl) (Object) this;
-        pearl.level.playSound(null, pearl.getX(), pearl.getY(), pearl.getZ(), SoundEvents.ENDERMAN_TELEPORT, SoundSource.PLAYERS, 1.0f, 1.0f);
+        Entity entity = pearl.getOwner();
+        if (entity instanceof Player) {
+            Player player = (Player) entity;
+            // Получаем координаты игрока после телепортации
+            double x = player.getX();
+            double y = player.getY();
+            double z = player.getZ();
+
+            pearl.level.playSound(null, x, y, z, SoundEvents.ENDERMAN_TELEPORT, SoundSource.PLAYERS, 1.0f, 1.0f);
+        }
+
     }
+
 }
