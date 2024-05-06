5564341ba2cd3a60f52712a2c9aa873c46a3502d
Shi3ofreniR <t40411304@yandex.ru>
2024-05-07 03:03:54 +0700

    dev

--- src/main/java/mod/nero/tpsound/mixin/TpSoundMixin.java
+++ src/main/java/mod/nero/tpsound/mixin/TpSoundMixin.java
@@ -1,7 +1,10 @@
 package mod.nero.tpsound.mixin;
 
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.projectile.ThrownEnderpearl;
 import net.minecraft.world.phys.EntityHitResult;
+import net.minecraft.world.phys.HitResult;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -9,8 +12,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ThrownEnderpearl.class)
 public abstract class TpSoundMixin {
-    @Inject(method = "onHitEntity", at = @At("TAIL"))
-    private void tpsnd(EntityHitResult pResult, CallbackInfo ci){
-
+    @Inject(method = "onHit", at = @At("HEAD"))
+    private void tpsnd(HitResult pResult, CallbackInfo ci){
+        ThrownEnderpearl pearl = (ThrownEnderpearl) (Object) this;
+        pearl.level.playSound(null, pearl.getX(), pearl.getY(), pearl.getZ(), SoundEvents.ENDERMAN_TELEPORT, SoundSource.PLAYERS, 1.0f, 1.0f);
     }
 }
